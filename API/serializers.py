5f42702669a4d998abe3379c03d98f8f17eacdc7
guny <bliss94s@naver.com>
2021-11-11 12:41:54 +0900

    remove API dir

--- API/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import api
-
-
-class apiSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = api
-        fields = ['bong', 'start_date', 'end_date']

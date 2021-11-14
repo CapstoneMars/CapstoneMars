e863180e8ee6249e531515b42ce3a4e72c0214c4
guny <bliss94s@naver.com>
2021-11-15 02:31:10 +0900

    fourth

--- ROAD/urls.py
+++ ROAD/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from DB import views
-from LINE import views
+#from LINE import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('DB/', include('DB.urls')),
-    path('LINE/', include('LINE.urls')),
+    #path('LINE/', include('LINE.urls')),
     path('', views.chart.as_view()),
     path('newp/', views.newp.as_view()),
     path('ajax_method/', views.ajax_method, name='ajax_method'),

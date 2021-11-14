fd842c8171daba90d778a016c0b18d0b0804b6d3
Jeon Jongmin <38072189+wakkpu@users.noreply.github.com>
2021-11-15 01:34:54 +0900

    updated urls.py and views.py
    이전 commit은 잘못됨. 수정하였음

--- DB/views.py
+++ DB/views.py
@@ -5,6 +5,10 @@ from .serializers import apiSerializer_1, apiSerializer_2, apiSerializer_3, apiS
 from rest_framework.response import Response
 from rest_framework.generics import ListAPIView
 
+from django.shortcuts import render
+from django.views.generic import View
+from django.http import JsonResponse
+
 
 # ?start_date=2017-08-17&end_date=2017-08-20
 class Date_1D(viewsets.ModelViewSet):
@@ -101,3 +105,36 @@ class Lines(viewsets.ModelViewSet):
 
             line = result.objects.all()
             return line
+
+
+class chart(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'chartjs/index.html')
+
+
+class newp(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'newp/daeguri.html')
+
+
+def ajax_method(request):
+    # 날짜 인풋 데이터 가져오기
+
+    receive_s = request.GET.get('start_date')
+    receive_e = request.GET.get('end_date')
+    receive_i = request.GET.get('chart_interval')
+
+    receive_i = "Date_60"
+
+    print(receive_s)
+    print(receive_e)
+    print(receive_i)
+    # http://127.0.0.1:8000/DB/date_60/?start_date=2021-08-17&end_date=2021-08-20
+
+    # 지지저항선 값을 모델연결을 해서 욜로 가져옴
+
+    # 임시로 데이터 2개전송
+    # supportLine 누르면 num1부분의 값만 창에 띄워지도록 해놨어용
+    LineData = {'num1': 54000000, 'num2': 56000000, 'stri': receive_i}
+    # 여기로 라인데이터를 보내주시면 됩니다
+    return JsonResponse(LineData)

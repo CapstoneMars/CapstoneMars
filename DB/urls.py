0b52bb488caa4df1413324fe6ad8aad1a7e577ec
guny <bliss94s@naver.com>
2021-11-15 18:25:56 +0900

    임대권

--- DB/urls.py
+++ DB/urls.py
@@ -1,13 +1,7 @@
 from django.conf.urls import url, include
-from .views import Date_15, Date_60, Date_240, Date_1D, Lines
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register('date_15', Date_15, basename='date_15')
-router.register('date_60', Date_60, basename='date_60')
-router.register('date_240', Date_240, basename='date_240')
-router.register('date_1D', Date_1D, basename='date_1D')
-router.register('lines', Lines, basename='lines')
 
 urlpatterns = [
     url('', include(router.urls))
--- DB/views.py
+++ DB/views.py
@@ -8,103 +8,7 @@ from rest_framework.generics import ListAPIView
 from django.shortcuts import render
 from django.views.generic import View
 from django.http import JsonResponse
-
-
-# ?start_date=2017-08-17&end_date=2017-08-20
-class Date_1D(viewsets.ModelViewSet):
-    serializer_class = apiSerializer_1
-    pagination_class = None
-    queryset = one_day.objects.all()
-
-    def get_queryset(self):
-
-        if self.request.method == "GET":
-
-            start_date = self.request.query_params.get('start_date')
-            end_date = self.request.query_params.get('end_date')
-
-            time_filter = self.queryset.filter(time__range=(
-                start_date, end_date))
-
-            result = apiSerializer_1(time_filter, many=True).data
-
-            return result
-
-
-class Date_60(viewsets.ModelViewSet):
-    serializer_class = apiSerializer_2
-    pagination_class = None
-    queryset = one_hour.objects.all()
-
-    def get_queryset(self):
-
-        if self.request.method == "GET":
-
-            start_date = self.request.query_params.get('start_date')
-            end_date = self.request.query_params.get('end_date')
-
-            time_filter = self.queryset.filter(time__range=(
-                start_date, end_date))
-
-            result = apiSerializer_2(time_filter, many=True).data
-
-            return result
-
-
-class Date_240(viewsets.ModelViewSet):
-    serializer_class = apiSerializer_3
-    pagination_class = None
-    queryset = four_hours.objects.all()
-
-    def get_queryset(self):
-
-        if self.request.method == "GET":
-
-            start_date = self.request.query_params.get('start_date')
-            end_date = self.request.query_params.get('end_date')
-
-            time_filter = self.queryset.filter(time__range=(
-                start_date, end_date))
-
-            result = apiSerializer_3(time_filter, many=True).data
-
-            return result
-
-# ?start_date=2021-10-09T00:00:00Z&end_date=2021-10-09T06:30:00Z
-
-
-class Date_15(viewsets.ModelViewSet):
-    serializer_class = apiSerializer_4
-    pagination_class = None
-    queryset = fifteen_min.objects.all()
-
-    def get_queryset(self):
-
-        if self.request.method == "GET":
-
-            start_date = self.request.query_params.get('start_date')
-            end_date = self.request.query_params.get('end_date')
-
-            time_filter = self.queryset.filter(time__range=(
-                start_date, end_date))
-
-            result = apiSerializer_4(time_filter, many=True).data
-
-            return result
-
-
-class Lines(viewsets.ModelViewSet):
-    serializer_class = resultSerializer
-
-    # def post(self, request):
-    # 알고리즘 결과물 json 으로 저장해서 post
-
-    def get_queryset(self):
-
-        if self.request.method == "GET":
-
-            line = result.objects.all()
-            return line
+import json
 
 
 class chart(View):
@@ -112,24 +16,37 @@ class chart(View):
         return render(request, 'chartjs/index.html')
 
 
-class newp(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'newp/daeguri.html')
-
-
 def ajax_method(request):
     # 날짜 인풋 데이터 가져오기
-
     receive_s = request.GET.get('start_date')
     receive_e = request.GET.get('end_date')
     receive_i = request.GET.get('chart_interval')
 
-    receive_i = "Date_60"
-
     print(receive_s)
     print(receive_e)
     print(receive_i)
-    # http://127.0.0.1:8000/DB/date_60/?start_date=2021-08-17&end_date=2021-08-20
+    if receive_i == "15M":
+        serializer_class = apiSerializer_4
+        queryset = fifteen_min.objects.all()
+        time_filter = queryset.filter(time__range=(receive_s, receive_e))
+        result = apiSerializer_4(time_filter, many=True).data
+    elif receive_i == "1H":
+        serializer_class = apiSerializer_2
+        queryset = one_hour.objects.all()
+        time_filter = queryset.filter(time__range=(receive_s, receive_e))
+        result = apiSerializer_2(time_filter, many=True).data
+    elif receive_i == "4H":
+        serializer_class = apiSerializer_3
+        queryset = four_hours.objects.all()
+        time_filter = queryset.filter(time__range=(receive_s, receive_e))
+        result = apiSerializer_3(time_filter, many=True).data
+    else:
+        serializer_class = apiSerializer_1
+        queryset = one_day.objects.all()
+        time_filter = queryset.filter(time__range=(receive_s, receive_e))
+        result = apiSerializer_1(time_filter, many=True).data
+
+    print(result)
 
     # 지지저항선 값을 모델연결을 해서 욜로 가져옴
 
@@ -138,3 +55,62 @@ def ajax_method(request):
     LineData = {'num1': 54000000, 'num2': 56000000, 'stri': receive_i}
     # 여기로 라인데이터를 보내주시면 됩니다
     return JsonResponse(LineData)
+
+
+def getChart(request):
+    # 날짜 인풋 데이터 가져오기
+    receive_s = request.GET.get('start_date')
+    receive_e = request.GET.get('end_date')
+    receive_i = request.GET.get('chart_interval')
+
+    print(receive_s)
+    print(receive_e)
+    print(receive_i)
+    if receive_i == "15M":
+        serializer_class = apiSerializer_4
+        queryset = fifteen_min.objects.all()
+        time_filter = queryset.filter(time__range=(receive_s, receive_e))
+        result = apiSerializer_4(time_filter, many=True).data
+    elif receive_i == "1H":
+        serializer_class = apiSerializer_2
+        queryset = one_hour.objects.all()
+        time_filter = queryset.filter(time__range=(receive_s, receive_e))
+        result = apiSerializer_2(time_filter, many=True).data
+    elif receive_i == "4H":
+        serializer_class = apiSerializer_3
+        queryset = four_hours.objects.all()
+        time_filter = queryset.filter(time__range=(receive_s, receive_e))
+        result = apiSerializer_3(time_filter, many=True).data
+    else:
+        serializer_class = apiSerializer_1
+        queryset = one_day.objects.all()
+        time_filter = queryset.filter(time__range=(receive_s, receive_e))
+        result = apiSerializer_1(time_filter, many=True).data
+
+    mon = {'01': "Jan",  '02': 'Feb', '03':  'Mar', '04': 'Apr', '05': 'May', '06': 'Jun',
+           '07':  'Jul', '08':  'Aug', '09':  'Sep', '10':  'Oct', '11': 'Nov', '12':  'Dec'}
+
+    for i in range(len(result)):
+        st = result[i]['time']
+        year = st[0:4]
+        b = mon[st[5:7]]
+        c = st[8:10]
+        d = st[11:16]
+        str = c + " " + b + " " + year + " " + d + " Z"
+        result[i]['time'] = str
+    # 01 Oct 2021 00:00
+
+    # 지지저항선 값을 모델연결을 해서 욜로 가져옴
+    # Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec
+
+    # 임시로 데이터 2개전송
+    # supportLine 누르면 num1부분의 값만 창에 띄워지도록 해놨어용
+    LineData = {'num1': 54000000, 'num2': 56000000, 'stri': receive_i}
+
+    print(result)
+
+    jsonData = json.dumps(result)
+
+    print(jsonData)
+    # 여기로 라인데이터를 보내주시면 됩니다
+    return JsonResponse(jsonData, safe=False)
